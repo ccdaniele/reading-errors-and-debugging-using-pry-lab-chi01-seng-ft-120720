30b8923128b49a1ae491033dae8d57cb8f9223f3
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-19 17:38:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,7 @@ require 'pry'
 require_relative '../fix_using_tests/pemdos'
 
 def snake_it_up(string)
-  if string[0] == "s"
-      binding.pry
+  if string[1] == "s"
     10 * "s" + string
   else
     string
